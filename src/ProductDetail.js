627fc95c244ea1709ce19d8d49b828cd2871c23f
janatvara <natwara.jaratvithitpong@mail.kmutt.ac.th>
2018-11-11 22:52:39 +0700

    header ProductDetail

--- src/ProductDetail.js
+++ src/ProductDetail.js
@@ -5,11 +5,27 @@ import './css/ProductDetail.css'
 
 class ProductDetail extends Component {
 
+
+detail = {
+	DataArray:[]
+}
+
+componentDidMount(){
+	fetch('http://localhost:8080/ProductDetail').then(res => console.log())
+}
+
+
   render() {
     return (
       <div>
         <Header name={'รายละเอียดสินค้า'} />
-
+         <nav class="header" >
+    		<nav class="col-xs-12 text-center" style={{background-color: '#CCD3F7'}}>
+       
+       		<a href="#" class="button"><img src="arrow.png" width="35" align="left" style={{margin-top: 10px ; margin-left: 4px}}/></a>
+          <h1>รายละเอียดสินค้า</h1>     
+     </nav>
+  </nav>
 
 
     

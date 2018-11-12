5ba90039596a015dfd01c99d1236edeecc829fc6
auum <a_auum@hotmail.com>
2018-11-12 21:37:16 +0700

    -AddAddress -Addcredit

--- src/AddCredit.js
+++ src/AddCredit.js
@@ -11,9 +11,42 @@ class AddCredit extends Component {
 
       <div>
         <Header name={'ใส่เลขบัตรเครดิต'} />
+       
+      <form class="form-inline">
+       <div class="container-fluid">
+        <div class="row text-center" style={{margin: '10px'}}>
+          <table width="100%"> 
+    <tr> <th > <h2 style={{fontSize: '18px'}}>ชื่อ - นามสกุล</h2></th> 
+    <th> <input type="text" class="form-control" id="NameSurname"  style={{width: '180px'}} /></th> </tr>
+    
+    <tr> <th > <h2 style={{fontSize: '18px'}}>เบอร์</h2></th> 
+    <th> <input type="text" class="form-control" id="Telephone"  style={{width: '180px'}} />
+    </th> </tr>
+     <tr> <th > <h2 style={{fontSize: '18px'}}>รายละเอียดที่อยู่</h2></th> 
+    <th> <input type="text" class="form-control" id="address" style={{width: '180px', height: '180px'}} /></th> </tr>
+    
+    <tr> <th > <h2 style={{fontSize: '18px'}}>ตำบล / แขวง</h2></th> 
+    <th> <input type="text" class="form-control" id="subdistrict"  style={{width: '180px'}} /></th> </tr>
+    <tr> <th > <h2 style={{fontSize: '18px'}}>อำเภอ / เขต</h2></th> 
+    <th> <input type="text" class="form-control" id="city" style={{width: '180px'}} /></th> </tr>
+     <tr> <th > <h2 style={{fontSize: '18px'}}>จังหวัด</h2></th> 
+    <th> <input type="text" class="form-control" id="province"  style={{width: '180px'}} /></th> </tr>
+    <tr> <th> <h2 style={{fontSize: '18px'}}>Postcode</h2></th> 
+    <th> <input type="text" class="form-control" id="Postcode" style={{width: '180px'}} /></th> </tr>
+  </table>
+</div>
+<div>
+  
+ <div align="center"><button type="submit" class="submit" style={{width: '190px', height: '50px'}} > 
+ <h3 style={{fontSize: '18px',textAlign: 'center'}}> ยืนยัน </h3> </button></div>
+</div>
+</div>
+
+    
+            </form>
+         </div> 
 
 
-      </div>
     );
   }
 }
